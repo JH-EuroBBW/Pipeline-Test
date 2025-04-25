7f710738863cb3d7ca5880faf0c9f9888a054738
JH-EuroBBW <HermelingJ@eurobbwbitburg2.onmicrosoft.com>
2025-04-25 11:18:12 +0200

    Projektdateien hinzufügen.

---
+++ Pipeline/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Pipeline.Models;
+
+
+namespace Pipeline.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> option) : base(option)
+        {
+        }
+
+
+        public DbSet<Miniature> Miniatures { get; set; }
+    }
+}
---
+++ Pipeline/Models/Miniature.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Pipeline.Models
+{
+    public class Miniature
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [DisplayName("Table Ready")]
+        public Boolean TableReady { get; set; } = false;
+        public String Description { get; set; }
+    }
+}

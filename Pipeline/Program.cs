00ee14df5bd6dcc67bd92714c08878d82596c656
JH-EuroBBW <HermelingJ@eurobbwbitburg2.onmicrosoft.com>
2025-06-25 14:46:32 +0200

    test 1

--- Pipeline/Program.cs
+++ Pipeline/Program.cs
@@ -3,6 +3,8 @@ using Pipeline.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// test
+
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages();
